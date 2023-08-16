b667c79199a70004a959ed687a1eb7c5138277b5
shachar kinreich <shachar0528@gmail.com>
2023-08-16 23:48:58 +0300

    clean code and add style

--- app/ListContacts.js
+++ app/ListContacts.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
-import { FontAwesome5, Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 import {
     Text,
     View,
@@ -13,20 +13,19 @@ import {
 } from 'react-native';
 import * as Contacts from 'expo-contacts';
 
-const StyledHeader = "bg-white p-8  sm:px-2 sm:py-6 lg:p-2 xl:px-2 xl:py-2 items-center justify-center"
-const StyledContacts = "flex-1 items-center justify-center bg-white";
-const StyledContact = "w-90 bg-red  flex-1 items-center justify-center shadow rounded";
-const StyledContactNumber = 'text-500 flex-1 items-center justify-center text-md font-bold mt-4';
-const StyledLoading = "flex-1 items-center justify-center space-y-5";
-const StyledSearch = "flex-4 text-lg font-bold w-full  leading-16 placeholder-slate-1400 rounded-md py-3 pl-10 ring-1 ring-slate-100 shadow-sm"
-const StyledText = "text-slate-800"
-const styleUser = "space-y-14 "
-const styleUserViwe = "bg-indigo-50 shadow-md h-20 px-max  font-semibold rounded-md border border-slate-200 items-center justify-center"
+const StyledHeader = "bg-white p-8 sm:px-2 sm:py-6 lg:p-2 xl:px-2 xl:py-2 items-center justify-center"
+const StyledLink = "p-4 l padding items-center justify-center";
+const StyledNameText = "flex-auto padding flex space-x-5";
+const StyledLoading = "flex-auto p-20 items-center justify-center space-y-15";
+const StyledSearch = "flex-5 text-lg border font-bold w-full placeholder-slate-1400 rounded-md py-3 pl-10 ring-1 ring-slate-100"
+const StyledNoContacts = "p-4 l text-red-600 text-lg font-bold rounded-lg"
+const styleUser = "space-y-14"
+const StyledText = "text-left"
 
 const ContactList = (() => {
     const [error, setError] = useState(undefined);
-    const [masterData, setcontact] = useState([]); //say set main state 
-    const [filterData, setFilterData] = useState([]); // filter state
+    const [masterData, setcontact] = useState([]);
+    const [filterData, setFilterData] = useState([]); 
     const [isLoading, setIsLoading] = useState(false);
     const [searchText, setSearchText] = useState("");
 
@@ -50,7 +49,6 @@ const ContactList = (() => {
                         setError("no contacts faund.")
                     }
                     setIsLoading(false)
-
                 } else {
                     setError("Permission to access contact denied.")
                 }
@@ -64,7 +62,7 @@ const ContactList = (() => {
         if (text) {
             setIsLoading(true)
             const newData = masterData.filter((item) => {
-                let first = ''
+                let first =''
                 let last = ''
                 if (item.firstName) {
                     first = item.firstName?.toString().toUpperCase()
@@ -85,35 +83,36 @@ const ContactList = (() => {
     }
 
     const renderItem = (item) => (
-        <Link
-            className={"p-3 l bg-white shadow rounded-lg items-center justify-center"}
-            href={{
-                pathname: "/ProfileContact",
-                params: {
-                    name: item?.firstName == null ? 'Missing name' : item.firstName,
-                    lastName: item?.lastName == null ? '' : item.lastName,
-                    phone: item?.phoneNumbers == undefined || null
-                        ? []
-                        : item?.phoneNumbers[0]?.number
-                }
-            }}
-        >
-                <FontAwesome5 name="user-circle" size={24} color="black" />
-
-            <View className={"flex-auto flex space-x-5"}>
-                <Text className={"text-s border-b "}>
-                    {item?.firstName == null
-                        ? "update name in your contacts"
-                        : item.firstName}{" "}
-                    {item?.lastName == null ? null : item.lastName}
-                </Text>
-                <Text className={"color-green"} >
-                    {item?.phoneNumbers == undefined || null
-                        ? []
-                        : item?.phoneNumbers[0]?.number}
-                </Text>
-            </View>
-        </Link>
+        <View className={styleUser}>
+            <Link
+                className={StyledLink}
+                href={{
+                    pathname: "/ProfileContact",
+                    params: {
+                        firstName: item?.firstName == null ? 'Missing name' : item.firstName,
+                        lastName: item?.lastName == null ? '' : item.lastName,
+                        phone: item?.phoneNumbers == undefined || null
+                            ? []
+                            : item?.phoneNumbers[0]?.number
+                    }
+                }}
+            >
+                <FontAwesome5 name="user-circle" size={24} />
+                <View className={StyledNameText}>
+                    <Text className={StyledText}>
+                        {item?.firstName == null
+                            ? "update name in your contacts"
+                            : item.firstName}{" "}
+                        {item?.lastName == null ? null : item.lastName}
+                    </Text>
+                    <Text className={StyledText} >
+                        {item?.phoneNumbers == undefined || null
+                            ? []
+                            : item?.phoneNumbers[0]?.number}
+                    </Text>
+                </View>
+            </Link>
+        </View>
     );
 
     return (
@@ -121,7 +120,7 @@ const ContactList = (() => {
             <TextInput
                 className={StyledSearch}
                 autoCorrect={false}
-                placeholder='search...'
+                placeholder='  Search here...'
                 value={searchText}
                 onChangeText={text => searchFilter(text)}
                 disabled={isLoading}
@@ -137,7 +136,7 @@ const ContactList = (() => {
                     keyExtractor={(item, index) => index.toString()}
                     renderItem={({ item }) => (renderItem(item))}
                     ListEmptyComponent={() => (
-                        <Text class={StyledText}>No contacts found</Text>)}
+                        <Text className={StyledNoContacts}>No Contacts Found</Text>)}
                 />}
             <Text>{error}</Text>
             <StatusBar style="auto" />
--- app/ProfileContact.js
+++ app/ProfileContact.js
@@ -6,26 +6,31 @@ import {
     Text,
     SafeAreaView,
     Pressable
-
 } from 'react-native';
 
-const StyledHeader = "bg-white space-y-3 p-6 sm:px-8 sm:py-6 lg:p-2 xl:px-6 xl:py-6 items-center justify-center"
+const StyledHeader = 
+"bg-white space-y-3 p-6 sm:px-8 sm:py-6 lg:p-2 xl:px-6 xl:py-6 items-center justify-center"
 const StyledText = "p-4 l bg-white text-lg font-bold shadow rounded-lg"
 const StyledButton = "bg-black leading-6 py-3 px-4 rounded-lg"
+const StyledButtonText = "text-white text-lg"
+
 const ProfileContact = (() => {
-    const { name, lastName, phone } = useLocalSearchParams()
+    const { firstName, lastName, phone } = useLocalSearchParams()
 
     return (
         <SafeAreaView className={StyledHeader} >
             <EvilIcons name="user" size={100} color="black" />
-            <Text className={StyledText} >Name: {name} {lastName}</Text>
-            <Text className={StyledText} >Phone: {phone}</Text>
+            <Text className={StyledText} >
+                Name: {firstName} {lastName}
+            </Text>
+            <Text className={StyledText} >
+                Phone: {phone}
+            </Text>
             <Pressable className={StyledButton} onPress={() => router.back()}>
-                <Text className="text-white text-lg">
+                <Text className={StyledButtonText}>
                     Go Back
                 </Text>
             </Pressable>
-
         </SafeAreaView>
     );
 })
